b65168b41ea2bafa931bbc45063d8448aada30cd
Isaac Mejia <edisaac.mejia@gmail.com>
2018-08-16 20:04:24 -0500

    x

---
+++ config.rb
@@ -0,0 +1,13 @@
+
+
+wiki_options = {
+  :live_preview => false,
+  :allow_uploads => false,
+  :per_page_uploads => false,
+  :allow_editing => false,
+  :css => false,
+  :js => false,
+  :mathjax => false,
+  :h1_title => true
+} 
+Precious::App.set(:wiki_options, wiki_options)
